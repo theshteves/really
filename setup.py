e7d38a448f74bdd82034a1eca1700d3e45a1ed58
theshteves <kneisers@msu.edu>
2016-06-11 18:33:19 -0700

    Added setup & requirements for enhanced distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="Really",
+    description="The missing package for Python",
+    version="0.1",
+    author="Steven Kneiser",
+    author_email="stevenkneiser@gmail.com",
+    url="github.com/theshteves/really",
+    packages=["really"],
+)
+
