a3e97dd7b72866aa0b3f90a343a58e9144bceec7
theshteves <stevenkneiser@gmail.com>
2017-12-26 12:23:44 -0500

    Perparing 0.1 package

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 
 setup(name="Really",
+    packages=["really"],
     description="The missing package for Python",
     version="0.1",
     author="Steven Kneiser",
     author_email="stevenkneiser@gmail.com",
     url="github.com/theshteves/really",
-    packages=["really"],
+    keywords=["really", "funny", "notreally"],
 )
 
